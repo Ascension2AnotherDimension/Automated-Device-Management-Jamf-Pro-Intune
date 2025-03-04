6b8b16e50d29127c4cb50fa18424d15b01992487
Ascension2AnotherDimension <103389538+Ascension2AnotherDimension@users.noreply.github.com>
2025-03-04 09:32:12 -0800

    Create security_policy.sh

---
+++ security_policy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Security policy enforcement for macOS and Windows
+
+# Enable FileVault (macOS)
+sudo fdesetup enable
+
+#Enable BitLocker (Windows)
+#PowerShell command for BitLocker will go here (for Windows devices)
